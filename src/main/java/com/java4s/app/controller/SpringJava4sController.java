dcf62240cedea9a5081f63f6cda9dcd9b8ea29ad
balamurali <balamurali@balamurali-Inspiron-7370>
2019-07-04 20:59:20 +0530

    at 9;00

--- src/main/java/com/java4s/app/controller/SpringJava4sController.java
+++ src/main/java/com/java4s/app/controller/SpringJava4sController.java
@@ -41,7 +41,7 @@ public class SpringJava4sController {
 	public SpringJava4sDAO dao;
 	
 		
-	@RequestMapping(value = "/sendem")
+	@RequestMapping(value = "/sendemails")
 	   public String sendEmail() throws AddressException, MessagingException, IOException {
 		
 		sendmail();
